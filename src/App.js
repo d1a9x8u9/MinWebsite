2defc8613238824af197b87d22fac4df14cc68d6
d1a9x8u9 <daxu7803@gmail.com>
2018-05-23 00:08:28 -0700

    Added routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom'
 
 import Content from './components/Content/Content'
 import classes from './App.css'
 
+
 class App extends Component {
   render() {
     return (
+      
       <div className={classes.App} >
-          <Content />        
+          <Switch>
+            <Route exact path='/' component={Content}/>
+          </Switch>        
       </div>
     );
   }
